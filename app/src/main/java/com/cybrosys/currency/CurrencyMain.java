bb9e83caa55a368c4958ba4d775c02d91a1fcf90
Adam Howard <achdisposable1+github@gmail.com>
2019-05-29 13:39:36 +0100

    migrate CurrencyMain to AndroidX
    class was omitted in earlier migration,
    because it was temporarily deleted

--- app/src/main/java/com/cybrosys/currency/CurrencyMain.java
+++ app/src/main/java/com/cybrosys/currency/CurrencyMain.java
@@ -37,7 +37,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.preference.PreferenceManager;
-import android.support.v4.app.Fragment;
+import androidx.fragment.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
